a312c117565fda121c6d582057a668ea7ea37e23
peterrayshen <peterrayshen@gmail.com>
2017-01-16 18:58:58 -0500

    test

---
+++ desktop/src/com/mygdx/battlefront/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.battlefront.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.battlefront.Battlefront;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Battlefront(), config);
+	}
+}
